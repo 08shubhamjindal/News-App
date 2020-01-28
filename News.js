2bd98a8ac36ae025d349efe136f47365db8008a0
Shubham Jindal <shubhamjindal.cse19@jecrc.ac.in>
2020-01-28 17:07:55 +0530

    Added Search functionality and Longest Common SubSequence for search results

--- News.js
+++ News.js
@@ -1,3 +1,32 @@
+var isoCountries  = {
+  'india' : 'in',
+  'canada' : 'ca',
+  'iceland' : 'is'
+};
+var countries = [
+  "india",
+  "canada",
+  "iceland"
+];
+function lcs(inputCountry, listCountry, inputCountrySize, listCountrySize) {
+  var x = new Array(inputCountrySize + 1);
+  for (var i = 0; i < x.length; i++) {
+       x[i] = new Array(listCountrySize + 1);
+     }
+  for (var i = 0; i <= inputCountrySize; i++){
+      for (var j = 0; j <= listCountrySize; j++){
+      if (i == 0 || j == 0)
+          x[i][j] = 0;
+
+      else if (inputCountry[i - 1] == listCountry[j - 1])
+          x[i][j] = x[i - 1][j - 1] + 1;
+
+      else
+          x[i][j] = Math.max(x[i - 1][j], x[i][j - 1]);
+      }
+  }
+  return x[inputCountrySize][listCountrySize];
+}
   window.onload = function() {
     localStorage.setItem("myCountryCode", 'us');
     localStorage.setItem("myCategoryCode", 'general');
@@ -21,9 +50,22 @@
         var countryCode = localStorage.getItem("myCountryCode");
         showData(countryCode, categoryCode);
   }
+  function getCountryCodeBySearch() {
+    var variable = document.getElementById('CountryCodeID_BySearch').value;
+    var maxi = 0;
+    var finalcountry;
+    for (var i = 0; i < countries.length; i++) {
+          var yy  = lcs(variable, countries[i], variable.length, countries[i].length);
+          if(yy>maxi){
+            maxi = yy;
+            finalcountry = countries[i];
+          }
+    }
+    console.log(finalcountry);
+    console.log(isoCountries[finalcountry]);
+    showData(isoCountries[finalcountry], 'general');
+  }
   function showData(countryCode, category) {
-  console.log(countryCode);
-  console.log(category);
   const ul = document.getElementById("news");
   ul.innerHTML = '';
   const url = 'https://newsapi.org/v2/top-headlines?country='+countryCode+'&category='+category+'&apiKey=a6d224d1b3044a3daa6d0f70856b41e0';
@@ -37,7 +79,7 @@
           img = createNode('img'),
           span = createNode('span');
       img.src = getarticle.urlToImage;
-      span.innerHTML = `<br><br/>${getarticle.author}<br><br/>${getarticle.description}<br><br/>`;
+      span.innerHTML = `<br><br/>${getarticle.author}<br><br/>${getarticle.content}<br><br/>`;
       append(li, img);
       append(li, span);
       append(ul, li);
